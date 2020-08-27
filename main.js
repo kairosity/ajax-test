dde357e14f35d7fc3667e749bf21615ee2d36bac
kairosity <karinafinegan@gmail.com>
2020-08-27 22:27:38 +0000

    add a cb function to get the data printed to console.

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-var xhr = new XMLHttpRequest(); //inbuilt js object to consume APIs
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+function getData(cb){ //this says this func takes a func as a param.
+    var xhr = new XMLHttpRequest(); //inbuilt js object to consume APIs
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
-xhr.send();
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //get used to get data from server
+    xhr.send(); //to send the above request
+
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText)); //this says put this data in the cb func. 
+        }
+    };
+
+}
+
+function printDataToConsole(data) { //another func that prints the data to console.
+    console.log(data);
+}
+
+getData(printDataToConsole); //calling the getdata func with the printdata func as the cb function. and that takes data as an arg. 
