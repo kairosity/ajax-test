cc28e9be26d690251cf90b82406e61f0bfab049d
kairosity <karinafinegan@gmail.com>
2020-08-28 13:21:51 +0000

    truncate the data to fit neatly into table

--- main.js
+++ main.js
@@ -14,8 +14,37 @@ function getData(type, cb){ //takes 2 params type and cb func.
 
 }
 
+function getTableHeaders(obj){
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key){ //Object.keys is a method - it will grab all the obj keys and for each key do the following: 
+        tableHeaders.push(`<td>${key}</td>`); //push them into the th arr and format them like so. 
+    });
+
+    return `<tr>${tableHeaders}</tr>`; //returns a row with all the headers 
+}
+
 function writeToDocument(type) { //func to write the results to the page - takes the type requested as a param. 
+    var tableRows = []; // empty tr arr
+    var el = document.getElementById("data"); //selects the div area to place the data in.
+    el.innerHTML = ""; // empty out the innerhtml.
+
     getData(type, function(data){ //calls the getData func with the type from above and the cb func looking for the data. That is the cb in the original func declaration the "data" param here is the JSON.parse(this.responseText) in the original.
-        document.getElementById('data').innerHTML = data; // sets the data response data into the data div on the webpage.
-    })
+        data = data.results; //redefines data to include the results - results??
+        var tableHeaders = getTableHeaders(data[0]); // the first element in the data will be the keys obj. 
+
+        data.forEach(function(item){ //for each bit of data do the following:
+            var dataRow = []; //create an empty array for each row.
+
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`); //for each key return the value pair and put it in a table cell.
+            })
+            tableRows.push(`<tr>${dataRow}</tr>`);
+            });
+
+            el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+        
 }
