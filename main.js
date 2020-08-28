4d92ee9ab67f5d4589cea475364c211542007597
kairosity <karinafinegan@gmail.com>
2020-08-28 10:55:23 +0000

    Refactor getData func using type

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
+const baseUrl = "https://ci-swapi.herokuapp.com/api/";
 
-function getData(cb){ //this says this func takes a func as a param.
+function getData(type, cb){ //takes 2 params type and cb func.
     var xhr = new XMLHttpRequest(); //inbuilt js object to consume APIs
 
-
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //get used to get data from server
+    xhr.open("GET", baseUrl + type); //get used to get data from server/ adds the type to the url to get full api request
     xhr.send(); //to send the above request
-
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -15,8 +14,8 @@ function getData(cb){ //this says this func takes a func as a param.
 
 }
 
-function printDataToConsole(data) { //another func that prints the data to console.
-    console.log(data);
+function writeToDocument(type) { //func to write the results to the page - takes the type requested as a param. 
+    getData(type, function(data){ //calls the getData func with the type from above and the cb func looking for the data. That is the cb in the original func declaration the "data" param here is the JSON.parse(this.responseText) in the original.
+        document.getElementById('data').innerHTML = data; // sets the data 
+    })
 }
-
-getData(printDataToConsole); //calling the getdata func with the printdata func as the cb function. and that takes data as an arg. 
